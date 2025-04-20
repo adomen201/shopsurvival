76ff4c8ceb0e82684f635db4e6e991c372ac133e
Adam <73246334+adomen201@users.noreply.github.com>
2025-04-20 18:16:40 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,87 @@
+// If you don't want the particles, change the following to false
+const doParticles = true;
+
+
+
+
+// Do not mess with the rest of this file unless you know what you're doing
+
+const getWidth = () => { // credit to travis on stack overflow
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+};
+
+if (doParticles) {
+    if (getWidth() < 400) $.firefly({
+        minPixel: 1,
+        maxPixel: 2,
+        total: 20
+    });
+    else $.firefly({
+        minPixel: 1,
+        maxPixel: 3,
+        total: 40
+    });
+}
+
+// This is for the click to copy
+let t;
+$(document).ready(() => {
+    t = $(".ip").html();
+});
+
+$(document).on("click", ".ip", () => {
+    let copy = document.createElement("textarea");
+    copy.style.position = "absolute";
+    copy.style.left = "-99999px";
+    copy.style.top = "0";
+    copy.setAttribute("id", "ta");
+    document.body.appendChild(copy);
+    copy.textContent = t;
+    copy.select();
+    document.execCommand("copy");
+    $(".ip").html("<span class='extrapad'>IP copied!</span>");
+    setTimeout(() => {
+        $(".ip").html(t);
+        var copy = document.getElementById("ta");
+        copy.parentNode.removeChild(copy);
+    }, 800);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const popupOverlay = document.getElementById('popupOverlay');
+    const popup = document.getElementById('popup');
+    const closePopup = document.getElementById('closePopup');
+    const infoButton = document.getElementById('info'); // Get the Info button element
+
+    // Function to open the popup
+    function openPopup() {
+        popupOverlay.style.display = 'block';
+    }
+
+    // Function to close the popup
+    function closePopupFunc() {
+        popupOverlay.style.display = 'none';
+    }
+
+    // Event listeners
+    // Trigger the popup to open when the Info button is clicked
+    infoButton.addEventListener('click', openPopup);
+
+    // Close the popup when the close button is clicked
+    closePopup.addEventListener('click', closePopupFunc);
+
+    // Close the popup when clicking outside the popup content
+    popupOverlay.addEventListener('click', function (event) {
+        if (event.target === popupOverlay) {
+            closePopupFunc();
+        }
+    });
+});
+
