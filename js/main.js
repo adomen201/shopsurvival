00d7c05e1938a273caa337b12c096f779343e744
Adam <73246334+adomen201@users.noreply.github.com>
2025-05-04 21:35:09 +0200

    Edited Rules page + placeholderrules

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// If you don't want the particles, change the following to false
 const doParticles = true;
 
 
@@ -53,35 +52,34 @@ $(document).on("click", ".ip", () => {
     }, 800);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
 
+document.addEventListener('DOMContentLoaded', function () {
     const popupOverlay = document.getElementById('popupOverlay');
     const popup = document.getElementById('popup');
     const closePopup = document.getElementById('closePopup');
-    const infoButton = document.getElementById('info'); // Get the Info button element
+    const infoButton = document.getElementById('info');
 
-    // Function to open the popup
+    if (!popupOverlay || !popup || !closePopup || !infoButton) return;
+
     function openPopup() {
         popupOverlay.style.display = 'block';
     }
 
-    // Function to close the popup
     function closePopupFunc() {
         popupOverlay.style.display = 'none';
     }
 
-    // Event listeners
-    // Trigger the popup to open when the Info button is clicked
     infoButton.addEventListener('click', openPopup);
-
-    // Close the popup when the close button is clicked
     closePopup.addEventListener('click', closePopupFunc);
-
-    // Close the popup when clicking outside the popup content
     popupOverlay.addEventListener('click', function (event) {
         if (event.target === popupOverlay) {
             closePopupFunc();
         }
     });
 });
-
